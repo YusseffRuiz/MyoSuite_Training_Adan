b4fac7f27aeb320e263fb133906640d2034e85f0
Yusseff <zeiverking@gmail.com>
2024-01-15 18:28:32 -0600

    updating main file with organised functions

--- baselines_DEPRL/main.py
+++ baselines_DEPRL/main.py
@@ -14,153 +14,201 @@ import matplotlib.pyplot as plt
 import cv2
 import os
 
+def modelCharacteristics(env, getNames=False):
+    # Testing of muscles and actuators
 
 
-
-
-# env = gym.make('myoLegWalk-v0', reset_type='random')
-# foldername = "baselines_DEPRL\deprl_baseline\myoLeg_Test_0210"
-foldername = "baselines_DEPRL\myoLegWalk_20230514\myoLeg"
-amp_foldername = "baselines_DEPRL\deprl_baseline\myo_amputation_1"
-# foldername = "baselines_DEPRL\deprl_baseline\myo_amputation_1"
-env_walk = "myoLegWalk-v0"
-env_walk_sar = "myoSarcLegWalk-v0"
-# 80 actions and muscles, 34 DoF
-env_rough = "myoLegRoughTerrainWalk-v0"
-env_chase = "myoChallengeChaseTagP1-v1"
-env_amp = "myoAmpWalk-v0"
-env_amp_sar = "myoSarcAmpWalk-v0"
-# 22 DoF 78 actions and muscles
-
-
-tot_episodes = 5
-visual = True
-randAction = False
-plotFlag = False
-ampFlag = False
-
-
-# TODO
-# Guardar cada uno de los pasos en frames para armar un video.
-# Describir como se entrenó al sistema de red neuronal, compuesto por 2 capas para cada uno de los músculos del cuerpo, con un sistema de entrenamiento MPO
-
-
-
-# Initialization to test action space
-
-if ampFlag:
-    env = gym.make(env_amp, reset_type="random")
-    policy = deprl.load(amp_foldername, env)
-    obs = env.reset()
+    position = env.sim.data.qpos.tolist()
+    velocity = env.sim.data.qvel.tolist()
+    muscles = env.sim.data.actuator_force.tolist()
+    tendons_len = env.sim.data.ten_length.tolist()
     action = env.action_space.sample()
-    next_state, reward, done, info = env.step(action)
-else:
-    env_w = gym.make(env_walk, reset_type="random")
-    policy_w = deprl.load(foldername, env_w)
-    obs_w = env_w.reset()
-    action_w = policy_w(obs_w)
 
-# Testing of muscles and actuators
+    print("total number of DoF in the model: ", env.sim.model.nv)
+    print("position: ", position)
 
-
-# position = env.sim.data.qpos.tolist()
-# velocity = env.sim.data.qvel.tolist()
-# muscles = env.sim.data.actuator_force.tolist()
-# muscle_length = env.sim.data.ten_length.tolist()
-
-# position_w = env_w.sim.data.qpos.tolist()
-# velocity_w = env_w.sim.data.qvel.tolist()
-# muscles_w = env_w.sim.data.actuator_force.tolist()
-# muscle_length_w = env_w.sim.data.ten_length.tolist()
-
-# print("total number of DoF in the model: ", env.sim.model.nv)
-# print("total number of DoF in the regular model: ", env_w.sim.model.nv)
-
-# print("Number of actions in the model: ", len(action))
-# print("Number of actions in the regular model: ", len(action_w))
-
-# print("Muscles: ", muscles)
-# print("Action: ", action)
-# print("total muscles: ", len(muscles))
-# print("total regular muscles: ", len(muscles_w))
+    print("total muscles: ", len(muscles))
+    print("Muscles: ", muscles)
+    print("total Tendons: ", tendons_len)
+    print("Number of actions in the model: ", len(action))
+    print("Action: ", action)
 
 
-# get the names of every group
-# for i in range(env.sim.model.ntendon):
-#    print('name of geom ', i, ' : ', env.sim.model.geom(i).name)
-
-"""
-Muscles are the one we need to keep tracking, action space only indicates the torque required to perform movement on the muscles.
-
-"""
-
-tb_ant_l = []
-psoas_l = []
-iliac_l = []
-muscle_length = []
-motor_action = []
+    if getNames:
+        #get the names of every group
+        for i in range(env.sim.model.ntendon):
+            print('name of geom ', i, ' : ', env.sim.model.geom(i).name)
 
 
-for ep in range(tot_episodes):
-    print(f"Episode: {ep + 1} of {tot_episodes}")
-    if ampFlag:
-        obs = env.reset()
-    else:
-        obs_w = env_w.reset()
-    done = False
-    while not done:
-        if randAction:
-            if ampFlag:
-                action = env.action_space.sample()
-                action = action * 0
-            else:
-                action_w = env_w.action_space.sample()
-                action_w = action_w * 0
-        else:
-            if ampFlag:
-                action = policy(obs)
-            else:
-                action_w = policy_w(obs_w)
-        if visual:
-            if ampFlag:
-                env.mj_render()
-            else:
-                env_w.mj_render()
-        # Render env output to video
-        if plotFlag:
-            muscles = env_w.sim.data.actuator_force.tolist()
-            tb_ant_l.append(muscles[75])
-            # psoas_l.append(muscles[68])
-            # iliac_l.append(muscles[64])
-            # motor_action.append(action[])
-        if ampFlag:
-            next_state, reward, done, info = env.step(action)
-            obs = next_state
-        else:
-            next_state, reward, done, info = env_w.step(action_w)
-            obs_w = next_state
-    print("Reward: ", reward)
-    if ampFlag:
-        env.close()
-    else:
-        env_w.close()
-
-if plotFlag:
-    if len(tb_ant_l) < 300:
+def plotMuscle(muscle, muscleName, colorValue, nSteps):
+    # colorValue = blue, yellow, red, green
+    nInit = 100
+    nEnd = nInit+nSteps
+    if len(muscle) < nEnd:
         print("Not enough Data, please repeat the experiment")
+        return True
     else:
-        tb_ant_l = tb_ant_l[200:300]
-        plt.plot(tb_ant_l, label="tb_ant_l", color="blue")
-        # plt.plot(psoas_l, label="illipsoas_l", color="red")
-        # plt.plot(iliac_l, label="illiacus_l", color="green")
+        muscleData = muscle[nInit:nEnd]
+        plt.plot(muscleData, label=muscleName, color=colorValue)
         plt.xlabel("Timesteps")
-        plt.ylabel("Muscle")
+        plt.ylabel("Torque")
+        plotName = "muscle " + muscleName + " force during a step"
         plt.title("Muscle force and muscle")
         plt.legend()
         plt.show()
 
 
-print("Process Finished")
+def oneRun(env, visual, plotFlag, randAction, policy, T):
+    if plotFlag:
+        gastroc_r = []
+        soleus_r = []
+        hip_flexion = []
+        plantar_flexion = []
+        knee_flexion = []
+        muscle_length = []
+        motor_action = []
+
+    obs = env.reset()
+
+    for ep in range(T):
+        if randAction:
+            action = env.action_space.sample()
+            action = action * 0
+        else:
+            action = policy(obs)
+        if visual:
+            env.mj_render()
+            # Render env output to video
+        if plotFlag:
+            muscles = env.sim.data.actuator_force.tolist()
+            position = env.sim.data.qpos.tolist()
+            gastroc_r.append(muscles[13])
+            soleus_r.append(muscles[33])
+            hip_flexion.append(position[7])
+            plantar_flexion.append(position[10])
+            knee_flexion.append(position[11])
+            # motor_action.append(action[])
+        next_state, reward, done, info = env.step(action)
+        obs = next_state
+    print("Reward: ", reward)
+    env.close()
+
+def multipleRun(env, visual, plotFlag, randAction, policy, totEpisodes):
+    if plotFlag:
+        gastroc_r = []
+        soleus_r = []
+        hip_flexion = []
+        plantar_flexion = []
+        knee_flexion = []
+        muscle_length = []
+        motor_action = []
+
+    obs = env.reset()
+
+    for ep in range(totEpisodes):
+        print(f"Episode: {ep + 1} of {totEpisodes}")
+        obs = env.reset()
+        done = False
+        while not done:
+            if randAction:
+                action = env.action_space.sample()
+                action = action * 0
+            else:
+                action = policy(obs)
+            if visual:
+                env.mj_render()
+            # Render env output to video
+            if plotFlag:
+                muscles = env.sim.data.actuator_force.tolist()
+                position = env.sim.data.qpos.tolist()
+                gastroc_r.append(muscles[13])
+                soleus_r.append(muscles[33])
+                hip_flexion.append(position[7])
+                plantar_flexion.append(position[10])
+                knee_flexion.append(position[11])
+                # motor_action.append(action[])
+            next_state, reward, done, info = env.step(action)
+            obs = next_state
+        print("Reward: ", reward)
+    env.close()
+
+def main(env_string, visual, randAction, plotFlag, sarcFlag, samples, testFlag = False, tot_episodes=5, T=500):
+
+    #Sarcopedia Flag only replace "myo" with "myoSarc" the weakness on muscles is added automatically
+    if sarcFlag:
+        env_string = env_string.replace("myo", "myoSarc")
+        print("Walking with Sarcopenia Model")
+    else:
+        print("Walking with healthy Model")
 
 
-exit()
+
+    #Initialise environment
+    env = gym.make(env_string, reset_type="random")
+    obs = env.reset()
+    action = env.action_space.sample()
+
+
+    foldername = "baselines_DEPRL\myoLegWalk_20230514\myoLeg"
+    amp_foldername = "baselines_DEPRL\deprl_baseline\myo_amputation_1"
+
+    if not randAction:
+        policy = deprl.load_baseline(env)
+    else:
+        policy = None
+
+    """
+    Muscles are the one we need to keep tracking, action space only indicates the torque required to perform movement on the muscles.
+
+    """
+
+    if testFlag:
+        oneRun(env, visual, plotFlag, randAction, policy, T)
+    else:
+        multipleRun(env, visual, plotFlag, randAction, policy, tot_episodes)
+
+    failed = False
+
+    if plotFlag:
+        plotMuscle(gastroc_r, "gastrocnemous medial right", "blue",samples)
+        plotMuscle(soleus_r, "soleus right left", "red", samples)
+        plotMuscle(hip_flexion, "hip flexion", "green", samples)
+        plotMuscle(knee_flexion, "knee flexion", "red", samples)
+        failed = plotMuscle(plantar_flexion, "plantar flexion", "blue", samples)
+
+    if failed:
+        return True
+    else:
+        return False
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+
+     visual = True # Visual mujoco representation
+     randAction = True # Just for testing random movements
+     plotFlag = False # Enable if we want plots of muscles and joint movement
+     sarcFlag = False # Sarcopenia on the model enabled or not
+     samples = 300 # how many samples do we want to get from the plots, if plotFlag is active
+     testFlag = False
+
+     env_walk = "myoLegWalk-v0"
+     # 80 actions and muscles, 34 DoF
+     env_rough = "myoLegRoughTerrainWalk-v0"
+     env_chase = "myoChallengeChaseTagP1-v1"
+     env_amp = "myoAmpWalk-v0"
+     # 34 DoF 78 actions and muscles
+
+     failed = True ## Loop to get graphs if model falls down, repeating until gathering required samples
+     while failed:
+         failed = main(env_string=env_amp, visual = visual, randAction = randAction, plotFlag = plotFlag, sarcFlag = sarcFlag, samples = samples, testFlag=testFlag)
+
+
+
+     #env = gym.make(env_walk, reset_type="random")
+     #modelCharacteristics(env)
+     print("Process Finished")
+
+     exit()
